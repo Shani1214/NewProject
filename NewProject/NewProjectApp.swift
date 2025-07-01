175a2180e7638200b8813a8d14d33e133ae60fe6
Shani1214 <zeshanhaider2942@gmail.com>
2025-07-01 12:25:30 +0500

    Initial Commit

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by MacMini on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
